77260c4b65a6e98501ddaaa8d112fb80f9082fd8
AmineELKAHIRI <elkahiriamine1999@gmail.com>
2022-11-25 22:53:08 +0100

    prepare the project for working on it

---
+++ src/main/java/com/example/springjokeprojectv2/SpringJokeProjectV2Application.java
@@ -0,0 +1,13 @@
+package com.example.springjokeprojectv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJokeProjectV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJokeProjectV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springjokeprojectv2/SpringJokeProjectV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjokeprojectv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokeProjectV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
