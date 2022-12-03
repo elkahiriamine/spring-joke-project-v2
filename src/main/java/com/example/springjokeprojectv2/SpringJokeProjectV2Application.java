34fcb71cd9340daa1e5b6b68cfaee0f7b53e2f6d
AmineELKAHIRI <elkahiriamine1999@gmail.com>
2022-12-03 14:05:12 +0100

    adding last modification

--- src/main/java/com/example/springjokeprojectv2/SpringJokeProjectV2Application.java
+++ src/main/java/com/example/springjokeprojectv2/SpringJokeProjectV2Application.java
@@ -2,8 +2,10 @@ package com.example.springjokeprojectv2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:spring-configure.xml")
 public class SpringJokeProjectV2Application {
 
     public static void main(String[] args) {
